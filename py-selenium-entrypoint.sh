8c058cd1b7eda4a8828bf8a31c3536e3c3b52ee3
Janusz Skonieczny <git@wooyek.com>
2017-11-05 21:22:55 +0100

    Bump version: 0.9.7 → 0.9.8

--- py-selenium-entrypoint.sh
+++ py-selenium-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "==============================================="
-echo "Docker image wooyek/py-selenium version: 0.9.7"
+echo "Docker image wooyek/py-selenium version: 0.9.8"
 echo "==============================================="
 
 echo "------> Setting up chromedriver path"
