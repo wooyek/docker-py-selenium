642f53bdd5345da6305d9546f28c70835ad99e6c
Janusz Skonieczny <git@wooyek.com>
2017-11-05 21:09:35 +0100

    show py env info

--- py-selenium-entrypoint.sh
+++ py-selenium-entrypoint.sh
@@ -2,12 +2,24 @@
 echo "==============================================="
 echo "Docker image wooyek/py-selenium version: 0.9.5"
 echo "==============================================="
+
 echo "------> Setting up chromedriver path"
 export PATH=$PATH:/usr/lib/chromium-browser/
 echo $PATH
 which chromedriver
 chromedriver --version
+dpkg -s chromium-chromedriver
 
-echo "------> Running command passed down to docker ***"
+echo "------> Python 2 info"
+which python2
+python2 -V
+pip2 list
+
+echo "------> Python 3 info"
+which python3
+python3 -V
+pip3 list
+
+echo "------> Running command passed down to docker"
 exec "$@"
 
