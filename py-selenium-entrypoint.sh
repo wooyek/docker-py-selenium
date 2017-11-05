8a53f60d6e3e96935fd76577a028e57b7928793b
Janusz Skonieczny <git@wooyek.com>
2017-11-05 14:53:25 +0100

    verbose path & version of chromedriver

--- py-selenium-entrypoint.sh
+++ py-selenium-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+echo "*** Setting up chromedriver path ***"
 export PATH=$PATH:/usr/lib/chromium-browser/
 echo $PATH
+which chromedriver
+chromedriver --version
 
 echo "*** Running command passed down to docker ***"
 exec "$@"
